ef0aab0144902f63c02952f10391f654175b26cb
Zoe <zmb6893@g.rit.edu>
2024-03-02 21:41:20 -0500

    Started implementation of calendar

--- frontend/dalendar/src/app/calendar-page/calendar-page.component.ts
+++ frontend/dalendar/src/app/calendar-page/calendar-page.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './calendar-page.component.css'
 })
 export class CalendarPageComponent {
-
+  months = [
+    
+  ]
 }
---
+++ frontend/dalendar/src/types/Calendar.ts
@@ -0,0 +1,19 @@
+export type CalendarMonth = {
+	name: Month,
+	days: number
+}
+
+export enum Month {
+	JAN="January",
+	FEB="February",
+	MAR="March",
+	APR="April",
+	MAY="May",
+	JUNE="June",
+	JULY="July",
+	AUG="August",
+	SEP="September",
+	OCT="October",
+	NOV="November",
+	DEC="December"
+}
