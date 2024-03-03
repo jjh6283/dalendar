61df6015eed0466bd890183e6c4bf4ce0b35c9b9
Zoe <zmb6893@g.rit.edu>
2024-03-02 20:00:38 -0500

    Added in the component imports

--- frontend/dalendar/src/app/app.component.ts
+++ frontend/dalendar/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalendarPageComponent } from './calendar-page/calendar-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FormComponentComponent } from './form-component/form-component.component';
+import { GoalsPageComponent } from './goals-page/goals-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalendarPageComponent, FormComponentComponent, GoalsPageComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
