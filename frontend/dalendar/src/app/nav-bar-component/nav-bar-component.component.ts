073c4c3fca1b97b884bd5160ed9df14d33547b42
Julia Han <jjh6283@rit.edu>
2024-03-02 19:58:56 -0500

    navbar

---
+++ frontend/dalendar/src/app/nav-bar-component/nav-bar-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-bar-component.component.html',
+  styleUrl: './nav-bar-component.component.css'
+})
+export class NavBarComponentComponent {
+
+}
